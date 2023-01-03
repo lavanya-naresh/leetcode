c209ddc4e20ba310f2708d11ab0081a2311d9da8
Lavanya Naresh <44650483+lavanya-naresh@users.noreply.github.com>
2023-01-03 13:15:55 +0530

    Update 944 - Delete Columns to Make Sorted.py

--- 944 - Delete Columns to Make Sorted.py
+++ 944 - Delete Columns to Make Sorted.py
@@ -4,12 +4,12 @@
 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        # bool flag list to indicate whether column should be removed or not
-        col_to_remove = [0] * len(strs[0])
+        # bool flag list to indicate whether column should be removed or not     
+        result = 0
         N, L = len(strs), len(strs[0])
         for col in range(L):
             for row in range(N):
-                if row + 1 < N and ord(strs[row][col]) > ord(strs[row + 1][col]):
-                    col_to_remove[col] = 1
+                if row + 1 < N and strs[row][col] > strs[row + 1][col]:
+                    result += 1
                     break
-        return sum(col_to_remove) 
+        return result
