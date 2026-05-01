a3a0196173e9aa8d6e8457bd380ecaba9b371938
lavanya-naresh <laksh112naresh@gmail.com>
2026-05-01 14:31:57 +0530

    solution for daily challenge problem 396 rotate function

---
+++ 396 DC.py
@@ -0,0 +1,24 @@
+'''
+Approach
+
+Brute force will not work because O(n ^ 2) time complexity will lead to TLE.
+To optimize we determine the relation between consecutive F(k) and F(k - 1) and use it to calculate F(k) from F(k - 1) in O(n) time.
+The relation is as follows:
+
+F(K) = F(K-1) + SUM(nums) - N * (nums[N - K])
+'''
+from typing import List
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        N = len(nums)
+        if N == 1:
+            return 0
+        best = 0
+        total_sum = sum(nums)
+        f_store = [0 for _ in range(N)]
+        f_store[0] = sum([i * nums[i] for i in range(N)])
+        best = f_store[0]
+        for k in range(1, N):
+            f_store[k] = f_store[k - 1] + total_sum - N * nums[N - k]
+            best = max(best, f_store[k])        
+        return best
